2539d3fa8b3f52d73ee911001c38c5b78788e65e
SK Mehedi Hasan <skmehedihasan.jr1@gmail.com>
2024-12-16 22:26:11 +0600

    new connection with firebase

--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { FaBars } from "react-icons/fa"; // Importing icon from react-icons
 import { IoIosArrowForward } from "react-icons/io";
 import { Link } from "react-router-dom";
-import ResortDirectory from "../../pages/ResortDirectory/ResortDirectory";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false); // State to control the dropdown menu
@@ -11,99 +10,99 @@ const Header = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  const closeMenu = () => {
+    setIsMenuOpen(false); // Close the menu when an item is clicked
+  };
+
   return (
     <>
-    <header className="bg-[#18294B] shadow-md">
-      <div className="container mx-auto flex items-center justify-between py-4 px-4 md:px-8">
-        {/* Logo */}
-        <div className="text-xl font-bold text-primary">
-          <a href="/" className="flex items-center space-x-2">
-            
-            <span className="text-white text-3xl">interval</span>
-          </a>
+      <header className="bg-[#18294B] shadow-md">
+        <div className="container mx-auto flex items-center justify-between py-4 px-4 md:px-8">
+          {/* Logo */}
+          <div className="text-xl font-bold text-primary">
+            <a href="/" className="flex items-center space-x-2">
+              <span className="text-white text-3xl">interval</span>
+            </a>
+          </div>
+
+          {/* Navigation for Desktop */}
+          <nav className="hidden md:flex space-x-6">
+            <a href="#home" className="text-neutral hover:text-primary">
+              Home
+            </a>
+            <a href="#about" className="text-neutral hover:text-primary">
+              About
+            </a>
+            <a href="#services" className="text-neutral hover:text-primary">
+              Services
+            </a>
+            <a href="#contact" className="text-neutral hover:text-primary">
+              Contact
+            </a>
+          </nav>
+
+          {/* Hamburger Menu for Mobile */}
+          <div className="md:hidden relative">
+            <button
+              onClick={toggleMenu}
+              className="btn btn-ghost btn-circle"
+              aria-label="Toggle Menu"
+            >
+              <FaBars className="h-6 w-6 text-white" />
+            </button>
+
+            {isMenuOpen && (
+              <ul className="absolute right-0 mt-2 w-64 bg-base-100 shadow-lg rounded-md z-50">
+                <div className="bg-[#18294B] border-t">
+                  <p className="text-white text-center font-semibold py-2">Menu</p>
+                </div>
+
+                <Link to="/" onClick={closeMenu}>
+                  <div className="border-t py-2 px-3 hover:bg-slate-300 flex justify-between">
+                    <p>Home</p>
+                    <IoIosArrowForward className="font-bold text-xl text-orange-500" />
+                  </div>
+                </Link>
+
+                <Link to="/login" onClick={closeMenu}>
+                  <div className="border-t py-2 px-3 hover:bg-slate-300 flex justify-between">
+                    <p>Login</p>
+                    <IoIosArrowForward className="font-bold text-xl text-orange-500" />
+                  </div>
+                </Link>
+
+                <Link to="/resort-directory" onClick={closeMenu}>
+                  <div className="border-t py-2 px-3 hover:bg-slate-300 flex justify-between">
+                    <p>Resort Directory</p>
+                    <IoIosArrowForward className="font-bold text-xl text-orange-500" />
+                  </div>
+                </Link>
+
+                <Link to="/create-profile" onClick={closeMenu}>
+                  <div className="border-t py-2 px-3 hover:bg-slate-300 flex justify-between">
+                    <p>Create a Profile</p>
+                    <IoIosArrowForward className="font-bold text-xl text-orange-500" />
+                  </div>
+                </Link>
+
+                <Link to="/join-today" onClick={closeMenu}>
+                  <div className="border-t py-2 px-3 hover:bg-slate-300 flex justify-between">
+                    <p>Join Today</p>
+                    <IoIosArrowForward className="font-bold text-xl text-orange-500" />
+                  </div>
+                </Link>
+
+                <Link to="/change-language" onClick={closeMenu}>
+                  <div className="border-y py-2 px-3 hover:bg-slate-300 flex justify-between">
+                    <p>Change Language</p>
+                    <IoIosArrowForward className="font-bold text-xl text-orange-500" />
+                  </div>
+                </Link>
+              </ul>
+            )}
+          </div>
         </div>
-
-        {/* Navigation for Desktop */}
-        <nav className="hidden md:flex space-x-6">
-          <a href="#home" className="text-neutral hover:text-primary">
-            Home
-          </a>
-          <a href="#about" className="text-neutral hover:text-primary">
-            About
-          </a>
-          <a href="#services" className="text-neutral hover:text-primary">
-            Services
-          </a>
-          <a href="#contact" className="text-neutral hover:text-primary">
-            Contact
-          </a>
-        </nav>
-
-        {/* Hamburger Menu for Mobile */}
-        <div className="md:hidden relative">
-          <button
-            onClick={toggleMenu}
-            className="btn btn-ghost btn-circle "
-            aria-label="Toggle Menu"
-          >
-            <FaBars className="h-6 w-6 text-white" />
-          </button>
-
-          {isMenuOpen && (
-            <ul className="absolute right-0 mt-2 w-64 bg-base-100 shadow-lg rounded-md z-50">
-
-               <div className="bg-[#18294B] border-t"> 
-                    <p className="text-white py-2">Menu</p>
-               </div> 
-
-              <Link to="/">
-               <div className='border-t py-2 px-3 hover:bg-slate-300 flex justify-between'> <p>Home</p>
-               <IoIosArrowForward className='font-bold text-xl text-orange-500' />
-               </div>
-               </Link>
-
-              <Link to="login">
-               <div className='border-t py-2 px-3 hover:bg-slate-300 flex justify-between'> <p>Login</p>
-               <IoIosArrowForward className='font-bold text-xl text-orange-500' />
-               </div>
-               </Link>
-
-               <Link to="resort-directory">
-               <div className='border-t py-2 px-3 hover:bg-slate-300 flex justify-between'> <p>Resort Directory</p>
-               <IoIosArrowForward className='font-bold text-xl text-orange-500' />
-               </div>
-               </Link>
-
-               <Link>
-               <div className='border-t py-2 px-3 hover:bg-slate-300 flex justify-between'> <p>Interval HD</p>
-               <IoIosArrowForward className='font-bold text-xl text-orange-500' />
-               </div>
-               </Link>
-
-               <Link to="create-profile">
-               <div className='border-t py-2 px-3 hover:bg-slate-300 flex justify-between'> <p>Create a Profile</p>
-               <IoIosArrowForward className='font-bold text-xl text-orange-500' />
-               </div>
-               </Link>
-
-               <Link>
-               <div className='border-y py-2 px-3 hover:bg-slate-300 flex justify-between'> <p>Join Today</p>
-               <IoIosArrowForward className='font-bold text-xl text-orange-500' />
-               </div>
-               </Link>
-
-               <Link>
-               <div className='border-y py-2 px-3 hover:bg-slate-300 flex justify-between'> <p>Change Language</p>
-               <IoIosArrowForward className='font-bold text-xl text-orange-500' />
-               </div>
-               </Link>
-
-
-            </ul>
-          )}
-        </div>
-      </div>
-    </header>
+      </header>
     </>
   );
 };
