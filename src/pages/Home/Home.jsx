7279cfd55ea367ccbddb5a30adc38e817cd1050c
SK Mehedi Hasan <skmehedihasan.jr1@gmail.com>
2024-12-27 15:55:35 +0600

    new condition for the home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,28 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Carousel from "./Carousel/Carousel";
 import Content from "./Content/Content";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const Home = () => {
-  
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      // Redirect authenticated user to the dashboard
+      navigate("/dashboard/gateways");
+    }
+  }, [user, navigate]);
+
   return (
     <div className="">
-      <Carousel/>
-      <Content/>
+      {!user && (
+        <>
+          <Carousel />
+          <Content />
+        </>
+      )}
     </div>
   );
 };
