c10402f1dfb0c033f00ada30a2fe88f15df90576
SK Mehedi Hasan <skmehedihasan.jr1@gmail.com>
2024-12-27 15:42:22 +0600

    sign out page

--- src/pages/Home/Content/Content.jsx
+++ src/pages/Home/Content/Content.jsx
@@ -1,43 +1,67 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { IoIosArrowForward } from 'react-icons/io';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../providers/AuthProvider';
 
 const Content = () => {
-     return (
-          <div className='pt-10'>
-               
-               <Link to="login">
-               <div className='border-t py-2 px-3 hover:bg-slate-300 flex justify-between'> <p>Login</p>
-               <IoIosArrowForward className='font-bold text-xl text-orange-500' />
-               </div>
-               </Link>
+  const { user, signOut } = useContext(AuthContext);
 
-               <Link to="resort-directory">
-               <div className='border-t py-2 px-3 hover:bg-slate-300 flex justify-between'> <p>Resort Directory</p>
-               <IoIosArrowForward className='font-bold text-xl text-orange-500' />
-               </div>
-               </Link>
+  const handlesignOut = () => {
+    signOut(); 
+  };
 
-               <Link>
-               <div className='border-t py-2 px-3 hover:bg-slate-300 flex justify-between'> <p>Interval HD</p>
-               <IoIosArrowForward className='font-bold text-xl text-orange-500' />
-               </div>
-               </Link>
-
-               <Link to="create-profile">
-               <div className='border-t py-2 px-3 hover:bg-slate-300 flex justify-between'> <p>Create a Profile</p>
-               <IoIosArrowForward className='font-bold text-xl text-orange-500' />
-               </div>
-               </Link>
-
-               <Link>
-               <div className='border-y py-2 px-3 hover:bg-slate-300 flex justify-between'> <p>Join Today</p>
-               <IoIosArrowForward className='font-bold text-xl text-orange-500' />
-               </div>
-               </Link>
-
+  return (
+    <div className="pt-10">
+      {user ? (
+        <div
+          className="border-t py-2 px-3 hover:bg-slate-300 flex justify-between cursor-pointer"
+          onClick={handlesignOut}
+        >
+          <p>LogOut</p>
+          <IoIosArrowForward className="font-bold text-xl text-orange-500" />
+        </div>
+      ) : (
+        <Link to="login">
+          <div className="border-t py-2 px-3 hover:bg-slate-300 flex justify-between">
+            <p>Login</p>
+            <IoIosArrowForward className="font-bold text-xl text-orange-500" />
           </div>
-     );
+        </Link>
+      )}
+
+      <Link to="resort-directory">
+        <div className="border-t py-2 px-3 hover:bg-slate-300 flex justify-between">
+          <p>Resort Directory</p>
+          <IoIosArrowForward className="font-bold text-xl text-orange-500" />
+        </div>
+      </Link>
+
+      <Link>
+        <div className="border-t py-2 px-3 hover:bg-slate-300 flex justify-between">
+          <p>Interval HD</p>
+          <IoIosArrowForward className="font-bold text-xl text-orange-500" />
+        </div>
+      </Link>
+
+      {!user && (
+        <>
+          <Link to="create-profile">
+            <div className="border-t py-2 px-3 hover:bg-slate-300 flex justify-between">
+              <p>Create a Profile</p>
+              <IoIosArrowForward className="font-bold text-xl text-orange-500" />
+            </div>
+          </Link>
+
+          <Link>
+            <div className="border-y py-2 px-3 hover:bg-slate-300 flex justify-between">
+              <p>Join Today</p>
+              <IoIosArrowForward className="font-bold text-xl text-orange-500" />
+            </div>
+          </Link>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Content;
