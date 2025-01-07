acc53736de8ddeaf378902c761de47e716069d48
SK Mehedi Hasan <skmehedihasan.jr1@gmail.com>
2025-01-08 01:06:59 +0600

    new update for loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -4,7 +4,9 @@ import loadingGif from "../assets/loading.gif"
 const Loading = () => {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-50">
-      <img src={loadingGif} alt="" />
+
+      <img src={loadingGif} alt="" className="min-w-64"/>
+
     </div>
   );
 };
