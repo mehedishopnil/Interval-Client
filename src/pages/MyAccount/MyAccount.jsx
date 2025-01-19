546b875822c695b69afd0690c193c58cc137398b
SK Mehedi Hasan <skmehedihasan.jr1@gmail.com>
2025-01-20 02:00:56 +0600

    new update for my profile

--- src/pages/MyAccount/MyAccount.jsx
+++ src/pages/MyAccount/MyAccount.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const MyAccount = () => {
      return (
-          <div>
-               <h1>This is My Account</h1>
+          <div className='p-2'>
+               <div>
+               <h1 className='text-xl font-bold text-[#0077BE]'>My Profile</h1>
+               <p className='pt-5'>Welcome
+			Sheraton,
+			 You may review all of your personal details and make changes to update your profile information.</p>
+               </div>
           </div>
      );
 };
