d0d0be15dc8c7863bebe39a16af1e1dbc2baaa8e
SK Mehedi Hasan <skmehedihasan.jr1@gmail.com>
2025-01-23 23:29:27 +0600

    checkout page

--- src/components/CheckoutPage/CheckoutPage.jsx
+++ src/components/CheckoutPage/CheckoutPage.jsx
@@ -30,21 +30,22 @@ const CheckoutPage = () => {
             alt={resort.resortName}
             className="rounded object-cover w-full sm:w-1/3 h-32 mb-4 sm:mb-0 sm:mr-4"
           />
-          <div className="text-center sm:text-left">
-            <h3 className="text-lg font-semibold">{resort.resortName}</h3>
-            <p className="text-gray-600">Usage: {card.usage}</p>
-            <p className="text-gray-600">Unit: {card.unit} (Lock-off capable)</p>
+          <div className=" sm:text-left">
+            <h3 className="text-lg font-bold text-center text-[#006eae] pb-4">{resort.resortName}</h3>
+            <p className="text-gray-600 font-bold">{card.usage}</p>
+            <p className="text-gray-600">Unit: {card.unit} </p>
+            <p>(Lock-off capable)</p>
           </div>
         </div>
       )}
 
       <div className="mb-6">
-        <h2 className="text-lg font-semibold mb-2">Select Option:</h2>
-        <p className="text-gray-700">1 Bedroom {card.unit} Full Kitchen</p>
+        <h2 className="text-lg font-semibold text-center mb-2">Select master or lock-off portion</h2>
+        <p className="text-gray-700 text-center border py-1 rounded">1 Bedroom {card.unit} Full Kitchen</p>
       </div>
 
       <button
-        className="bg-[#0077be] text-white py-2 px-4 rounded hover:bg-[#006eae]"
+        className="w-full bg-[#0077be] text-white py-2 px-4 rounded hover:bg-[#006eae]"
         onClick={handleContinue}
       >
         Continue
